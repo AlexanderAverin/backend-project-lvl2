70a224088649ad1c4f7590d77fcd0605c738d493
Alexander Averin <averin.alexander@outlook.com>
2021-08-09 17:33:46 +0300

    Second step

--- src/gendiffLogic.js
+++ src/gendiffLogic.js
@@ -5,7 +5,7 @@ const programm = new Command();
 programm
   .version('output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type] ', 'output format')
+  .option('-f, --format [type]', 'output format')
   .arguments('filepath1 filepath2');
 
 programm.parse(process.argv);
