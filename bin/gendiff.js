435e19bd181e53a15b0830e157f1f13526be47ff
Alexander Averin <averin.alexander@outlook.com>
2021-08-22 00:25:53 +0300

    Step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,37 @@
 #!/usr/bin/env node
 
-import programm from '../src/gendiffLogic.js';
+import { Command } from 'commander/esm.mjs';
+
+import * as fs from 'fs';
+import path from 'path';
+import { cwd } from 'process';
+import genDiff from '../src/genDiff.js';
+
+const programm = new Command();
+
+const isJson = (filepath) => path.extname(filepath) === '.json';
+
+const isYaml = (filepath) => path.extname(filepath) === '.yaml';
+
+programm
+  .version('output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('filepath1 filepath2')
+
+  .action((filepath1, filepath2) => {
+    const DirectoryPath = cwd(filepath1);
+    const DirectoryPath2 = cwd(filepath2);
+
+    const pathToFile = path.resolve(DirectoryPath, filepath1);
+    const pathToFile2 = path.resolve(DirectoryPath2, filepath2);
+    if (isJson(pathToFile) && isJson(pathToFile)) {
+      const parsedFile = fs.readFileSync(pathToFile, 'utf8');
+      const parsedFile2 = fs.readFileSync(pathToFile2, 'utf8');
+      console.log(genDiff(JSON.parse(parsedFile), JSON.parse(parsedFile2)));
+    }
+  });
+
+programm.parse(process.argv);
+
+export default programm;
---
+++ src/genDiff.js
@@ -0,0 +1,82 @@
+import _ from 'lodash';
+
+const getSymbolStat = (status) => {
+  switch (status) {
+    case 'unchanged':
+      return { first: ' ', second: ' ' };
+    case 'deleted':
+      return { first: '-', second: '+' };
+    case 'added':
+      return { first: '+', second: '-' };
+    default: return { first: ' ', second: '' };
+  }
+};
+
+const getChildren = (tree) => tree.children;
+
+const getChild = (name, inFile1, inFile2, status) => ({ name, inFile1, inFile2, status });
+
+const getString = (status, name, value) => `${status} ${name}: ${value}`;
+
+const mergeKeys = (obj1, obj2) => [...Object.keys(obj1), ...Object.keys(obj2)];
+
+const Unchanged = (key, obj1, obj2) => {
+  return _.has(obj1, key) && _.has(obj2, key) && obj1[key] === obj2[key] ? 'unchanged' : false;
+};
+
+const Deleted = (key, obj1, obj2) => {
+  return _.has(obj1, key) && obj1[key] !== obj2[key] ? 'deleted' : false;
+};
+
+const Added = (key, obj1, obj2) => {
+  return _.has(obj2, key) && obj1[key] !== obj2[key] ? 'added' : false;
+};
+
+const getStatsMap = (key, obj1, obj2) => (func) => (func2) => (func3) => ({
+  unchanged: func(key, obj1, obj2),
+  added: func2(key, obj1, obj2),
+  deleted: func3(key, obj1, obj2),
+});
+
+const handler = (key, obj1, obj2) => {
+  const statsMap = getStatsMap(key, obj1, obj2)(Unchanged)(Deleted)(Added);
+  const runner = (keys) => {
+    const firstItem = keys[0];
+    if (statsMap[firstItem]) {
+      return statsMap[firstItem];
+    }
+    return runner(keys.slice(1, keys.length));
+  };
+  return runner(Object.keys(statsMap));
+};
+
+const generateTree = (obj1, obj2) => {
+  const keys = mergeKeys(obj1, obj2);
+  const tree = {};
+
+  const iter = (mergedKeys) => keys.reduce((acc, key) => {
+    const children = !_.isObject(obj1[key]) && !_.isObject(obj2[key]) ? [] : generateTree(obj1[key], obj2[key]);
+    return [...acc, { name: key, inFile1: obj1[key], inFile2: obj2[key], children, status: handler(key, obj1, obj2) }];
+  }, []);
+  tree.children = iter(keys);
+  return tree;
+};
+
+const render = (tree) => _.sortBy(getChildren(tree), (child) => child.name)
+  .reduce((acc, child) => {
+    const firstStatus = getSymbolStat(child.status).first;
+    const secondStatus = getSymbolStat(child.status).second;
+    const { name, inFile1, inFile2 } = child;
+
+    const str = getString(firstStatus, name, inFile1 ?? inFile2);
+    const str2 = inFile1 && inFile2 && inFile1 !== inFile2 ? getString(secondStatus, name, inFile2) : [];
+    return !acc.includes(str) ? [...acc, str, str2] : [...acc];
+  }, []).flat().join('\n');
+
+const genDiff = (file1, file2) => {
+  const tree = generateTree(file1, file2);
+  const renderTree = render(tree);
+  return `{\n${renderTree}\n}`;
+};
+
+export default genDiff;
