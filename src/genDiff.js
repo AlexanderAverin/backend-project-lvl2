239a56ae905b8bff79f1584c8f07cceb783da5e8
Alexander Averin <averin.alexander@outlook.com>
2021-08-29 11:52:15 +0300

    Update genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,39 +1,11 @@
-import _ from 'lodash';
-
-import parse from './parsers/parse.js';
-
-const getSymbolStat = (status) => {
-  switch (status) {
-    case 'unchanged':
-      return { firstStatus: ' ', secondStatus: ' ' };
-    case 'deleted':
-      return { firstStatus: '-', secondStatus: '+' };
-    case 'added':
-      return { firstStatus: '+', secondStatus: '-' };
-    default: return { firstStatus: ' ', secondStatus: '' };
-  }
-};
-
-const getChildren = (tree) => tree.children;
-
-const getString = (status, name, value) => `  ${status} ${name}: ${value}`;
-
-const render = (tree) => _.sortBy(getChildren(tree), (child) => child.name)
-  .reduce((acc, child) => {
-    const { firstStatus, secondStatus } = getSymbolStat(child.status);
-    const { name, inFile1, inFile2 } = child;
-
-    const str = getString(firstStatus, name, inFile1 ?? inFile2);
-    const str2 = inFile1 && inFile2 && inFile1 !== inFile2
-      ? getString(secondStatus, name, inFile2) : [];
-
-    return !acc.includes(str) ? [...acc, str, str2] : [...acc];
-  }, []).flat().join('\n');
+import generateAst from './parser.js';
+import render from './renders/visual.js';
 
 const genDiff = (file1, file2) => {
-  const tree = parse(file1, file2);
-  const renderTree = render(tree);
-  return `{\n${renderTree}\n}`;
+  const Ast = generateAst(file1, file2);
+  // return Ast;
+  const renderTree = render(Ast);
+  return renderTree;
 };
 
 export default genDiff;
